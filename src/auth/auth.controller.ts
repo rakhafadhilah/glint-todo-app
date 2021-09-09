2d646e51db8cf8f67704906a5d63b1950d413b25
Rakha Fadhilah <rakha.1243@gmail.com>
2021-09-09 20:37:22 +0700

    first deployment

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,12 +8,10 @@ import { LocalAuthGuard } from './guard/local-auth.guard';
 export class AuthController{
     constructor(private authService: AuthService){}
 
-    // @UseGuards(AuthGuard('local'))
     @UseGuards(LocalAuthGuard)
     @Post('login')
     loginUser(@Body('username') username: string, @Body('password') password: string){
         return this.authService.login(username, password)
-        // return this.authService.validateUser(username, password)
     }
     
     @UseGuards(LocalAuthGuard)
