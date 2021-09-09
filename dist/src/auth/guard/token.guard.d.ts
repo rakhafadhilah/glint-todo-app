605ef50fbedf5aaa7473ee44d69e006ed9de0923
Trias Bratayudhana <triasbrata@gmail.com>
2021-09-09 14:20:10 +0800

    Delete dist directory

--- dist/src/auth/guard/token.guard.d.ts
+++
@@ -1,4 +0,0 @@
-declare const TokenAuthGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class TokenAuthGuard extends TokenAuthGuard_base {
-}
-export {};
